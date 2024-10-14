cea3f00a4870b1dc8f1e9a36567c3dffa6fb2152
Nahid Hasan <nahidn228@gmail.com>
2024-10-14 22:45:50 +0600

    sotarge

---
+++ storage1.js
@@ -0,0 +1,13 @@
+const addToLocalStorage = () => {
+  const inputId = document.getElementById("storage-id");
+  const id = inputId.value;
+  const inputValue = document.getElementById("storage-value");
+  const value = inputValue.value;
+  if (id && value) {
+    //localStorage.setItem(id, value);
+    localStorage.setItem(id, JSON.stringify(value));
+  }
+
+  inputId.value = "";
+  inputValue.value = "";
+};
